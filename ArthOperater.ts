c961221f05221c08994de3ac2a470dd413553893
Aiman <aimanat2022@gmail.com>
2024-06-30 11:35:53 +0500

    first nb

---
+++ ArthOperater.ts
@@ -0,0 +1,10 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+//You should create four lines that look like this:
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+console.log("Addition",5 + 3);
+console.log("subraction",10-2);
+console.log("mulipticaltion",2*4);
+console.log("division",16/2);
